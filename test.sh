066828030638339e317d1c74dce3a6259b11e548
Yury <sidorenkoy@restream.rt.ru>
2019-03-17 18:42:32 +0300

    Added tests runner script.

---
+++ test.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# delete old '*gen.go' files
+find . -name '*gen.go' | xargs rm -rf -v
+
+# generate new '*gen.go' files
+egrep -l -R --include '*.go' "(go:generate)" . | sed 's/^/go generate /' | sh -v
+
+# run all the tests in the project
+find . -name '*_test.go' | xargs -n1 dirname | xargs go test
